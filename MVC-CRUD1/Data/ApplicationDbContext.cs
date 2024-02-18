cb64c8c834165f72bdccb61be51aca1f04869e06
janthony25 <junanthony.business@gmail.com>
2024-02-18 23:47:07 +1300

    Add ApplicationDbContext

---
+++ MVC-CRUD1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_CRUD1.Models;
+
+namespace MVC_CRUD1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
